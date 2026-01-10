faae37919b2641ca3dd8af291229ce0eb0ed37d6
Mariano Velamazán <mariano.velamazan@escueladeartedesevilla.es>
2026-01-10 19:08:35 +0100

    Add white background circle to logo in splash

--- src/CameraScreen.jsx
+++ src/CameraScreen.jsx
@@ -479,7 +479,7 @@ const CameraScreen = () => {
                         <div style={{ background: '#ffffff', borderRadius: '50%', width: 160, height: 160, display: 'flex', alignItems: 'center', justifyContent: 'center', margin: '0 auto 24px' }}>
                             <img src="/curiocity-logo.svg" alt="Curiocity" style={{ width: 128, height: 128 }} />
                         </div>
-                        <h1 style={{ color: '#000000', fontWeight: 900, fontSize: 28, margin: 0 }}>Curiocity</h1>
+                        
                         <p style={{ color: '#333', fontSize: 14, marginTop: 8 }}>Loading…</p>
                     </div>
                 </div>
